b0d5622e00b6696f944eca7c11b391a8e511e4bc
Jhon Doe <jaimemejia1003@gmail.com>
2024-04-15 23:13:39 -0500

    Refactor the code, in order to modularize the logic and not create very extensive components, <Form> and logic to bring the types of incidents from the database, save them in the global state of incidents and show it in the Select of the form if they are not undefined

---
+++ src/green-alert/components/form/FormInputs.tsx
@@ -0,0 +1,58 @@
+import { Input, Select, SelectItem } from "@nextui-org/react"
+import type { FormIncident, IncidentType } from "../../../types"
+
+interface FormInputsProps {
+    onChangeInputs: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void
+    formState: FormIncident
+    listOfTypeIncidents: IncidentType[]
+}
+
+export const FormInputs = ({ onChangeInputs, formState, listOfTypeIncidents }: FormInputsProps ) => {
+    return (
+        <>
+            <div className="flex flex-col gap-2 items-start mb-1 text-left">
+                <h1 style={{ color: "#17C964" }}>Nombre</h1>
+                <Input
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    labelPlacement="outside"
+                    style={{ textAlign: "left", width: "300px" }}
+                    name="name"
+                    // Guarda el nombre en el estado cada vez que cambia
+                    onChange={onChangeInputs}
+                    aria-label="Nombre de usuario"
+                    value={formState.name}
+                // errorMessage={error && error}
+                // isInvalid={!!error}
+                />
+            </div>
+
+            <div className="flex flex-col gap-2 text-left">
+                    <h1 style={{ color: "#17C964" }}>
+                        Tipo de Incidente
+                    </h1>
+
+                    <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
+                        <Select
+                            isRequired
+                            labelPlacement="outside"
+                            placeholder="Seleccionar"
+                            className="max-w-xs"
+                            style={{ width: "320px" }}
+                            value={formState.incident_type}
+                            name="incident_type"
+                            onChange={onChangeInputs}
+                            aria-label="Seleccionar incidente"
+                        >
+                            {listOfTypeIncidents.map((incidente) => (
+                                <SelectItem key={incidente.id} value={incidente.id}>
+                                    {incidente.name}
+                                </SelectItem>
+                            ))}
+                        </Select>
+                    </div>
+                </div>
+        </>
+    )
+}
+
--- src/store/Incidents/thunks.ts
+++ src/store/Incidents/thunks.ts
@@ -1,73 +1,89 @@
 import { supabase } from "../../utils/supabase";
+import { clearActiveIncident, clearIsLoading, deleteActiveIncident, loadIncidents, loadIncidentsTypes, setIsLoading, updateIncident } from ".";
 
 import type { AppDispatch } from "../store";
-import type { MarkerType } from "../../types";
-import { clearActiveIncident, clearIsLoading, deleteActiveIncident, loadIncidents, setIsLoading, updateIncident } from ".";
+import type { MarkerType, IncidentType } from "../../types";
+
+export const fetchDataIncidentTypes = () => {
+    return async (dispatch: AppDispatch) => {
+        const { data, error } = await supabase.from("incident_type").select()
+
+        if (error) return console.log(error);
+
+        const tempIncidentsTypes: IncidentType[] = [];
+
+        data.forEach((item: IncidentType) => {
+            tempIncidentsTypes.push(item);
+        });
+
+        dispatch(loadIncidentsTypes(tempIncidentsTypes))
+    }
+}
 
 export const fetchDataIncidents = () => {
-  return async (dispatch: AppDispatch) => {
-    const { data, error } = await supabase.from("incidents_duplicate").select();
+    return async (dispatch: AppDispatch) => {
+        const { data, error } = await supabase.from("incidents_duplicate").select();
 
-    if (error) return console.log(error);
+        if (error) return console.log(error);
 
-    const tempIncidents: MarkerType[] = [];
+        const tempIncidents: MarkerType[] = [];
 
-    data.forEach((item: MarkerType) => {
-      tempIncidents.push(item);
-    });
+        data.forEach((item: MarkerType) => {
+            tempIncidents.push(item);
+        });
 
-    dispatch(loadIncidents(tempIncidents));
-  };
+        dispatch(loadIncidents(tempIncidents));
+    };
 };
 
 export const uploadDataToDatabase = (dataToUpload: MarkerType) => {
-  return async (dispatch: AppDispatch) => {
+    return async (dispatch: AppDispatch) => {
 
-    dispatch(setIsLoading())
+        dispatch(setIsLoading())
 
-    const { data, error } = await supabase
-      .from("incidents_duplicate")
-      .insert(dataToUpload);
+        const { data, error } = await supabase
+            .from("incidents_duplicate")
+            .insert(dataToUpload);
 
-    console.log("Soy base de datoss");
-    if (error) {
-      console.error("Error subiendo datos:", error.message);
+        console.log("Soy base de datoss");
+        if (error) {
+            console.error("Error subiendo datos:", error.message);
 
-      dispatch(clearIsLoading())
-      dispatch(deleteActiveIncident())
-      return;
-    }
+            dispatch(clearIsLoading())
+            dispatch(deleteActiveIncident())
+            return;
+        }
 
-    console.log("Se subio exitosamente:", data);
+        console.log("Se subio exitosamente:", data);
 
-    dispatch(updateIncident(dataToUpload))
-    dispatch(clearActiveIncident())
-  };
+        dispatch(updateIncident(dataToUpload))
+        dispatch(clearActiveIncident())
+    };
 };
 
 export const updateDataToDatabase = (dataToUpdate: MarkerType) => {
-  return async (dispatch: AppDispatch) => {
+    return async (dispatch: AppDispatch) => {
 
-    dispatch(setIsLoading())
+        dispatch(setIsLoading())
 
-    console.log('Subiennnndo')
+        console.log('Subiennnndo')
 
-    const { data, error } = await supabase
-      .from("incidents_duplicate")
-      .update(dataToUpdate)
-      .eq("id", dataToUpdate.id);
-      
-    if (error) {
-      console.error("Error subiendo datos:", error.message);
-      dispatch(clearIsLoading())
-      dispatch(clearActiveIncident())
+        const { data, error } = await supabase
+            .from("incidents_duplicate")
+            .update(dataToUpdate)
+            .eq("id", dataToUpdate.id);
 
-      return;
-    }
+        if (error) {
+            console.error("Error subiendo datos:", error.message);
+            dispatch(clearIsLoading())
+            dispatch(clearActiveIncident())
 
-    dispatch(updateIncident(dataToUpdate))
-    dispatch(clearActiveIncident())
+            return;
+        }
 
-    console.log("Se subio exitosamente:", data);
-  };
+        dispatch(updateIncident(dataToUpdate))
+        dispatch(clearActiveIncident())
+
+        console.log("Se subio exitosamente:", data);
+    };
 };
