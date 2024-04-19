204d1227c44137c4726ad919fb3edc6587770a44
Sebastian <sebastian.hernandorv@gmail.com>
2024-04-18 23:49:20 -0500

    Interfaz login y registro

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,11 +1,57 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { BiSolidLockAlt, BiSolidUser } from "react-icons/bi";
+import { IoMail } from "react-icons/io5";
+import "../styles/AuthStyles.css";
 
 export const RegisterPage = () => {
-    return (
-        <div>
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Login Enviado");
+  };
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <form action="submit" onSubmit={handleOnSubmit}>
+          <h1>GreenAlert</h1>
+
+          <div className="input-box">
+            <input type="text" placeholder="Nombre de Usuario" required></input>
+            <BiSolidUser className="icon" />
+          </div>
+
+          <div className="input-box">
+            <input
+              type="text"
+              placeholder="Correo electronico"
+              required
+            ></input>
+            <IoMail className="icon" />
+          </div>
+
+          <div className="input-box">
+            <input type="password" placeholder="Contraseña" required></input>
+            <BiSolidLockAlt className="icon" />
+          </div>
+
+          <button type="submit">Registrarme</button>
+
+          <div className="register-link">
+            <p>
+              ¿Ya tienes una cuenta?{" "}
+              <Link className="a" to="/auth/login">
+                Iniciar ahora
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+/* <div>
             <p>Aca me voy a registrar</p>
 
             <Link to='/auth/login' >Ir al login</Link>
-        </div>
-    )
-}
+        </div> */
