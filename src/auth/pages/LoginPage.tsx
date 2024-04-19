8a9b1e46fe7ff7efaeaf03a70bd2a3c3b02e0211
Jhon Doe <jaimemejia1003@gmail.com>
2024-04-19 13:39:58 -0500

    fix error: Interface login/Register

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,57 +1,51 @@
 import { Link } from "react-router-dom";
 import { BiSolidLockAlt } from "react-icons/bi";
 import { IoMail } from "react-icons/io5";
+
 import "../styles/AuthStyles.css";
-import { color } from "framer-motion";
 
 export const LoginPage = () => {
-  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Login Enviado");
-  };
+    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Login Enviado");
+    };
 
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <form action="submit" onSubmit={handleOnSubmit}>
-          <h1>GreenAlert</h1>
+    return (
+        <main className="container">
+            <div className="wrapper">
+                <form action="submit" onSubmit={handleOnSubmit}>
+                    <h1>GreenAlert</h1>
 
-          <div className="input-box">
-            <input
-              type="text"
-              placeholder="Correo electronico"
-              required
-            ></input>
-            <IoMail className="icon" />
-          </div>
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            placeholder="Correo electronico"
+                            required
+                        ></input>
+                        <IoMail className="icon" />
+                    </div>
 
-          <div className="input-box">
-            <input type="password" placeholder="Contraseña" required></input>
-            <BiSolidLockAlt className="icon" />
-          </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Contraseña" required></input>
+                        <BiSolidLockAlt className="icon" />
+                    </div>
 
-          <button type="submit">Iniciar sesion</button>
+                    <button type="submit">Iniciar sesion</button>
 
-          <div className="register-link">
-            <p>
-              ¿No tienes una cuenta?{" "}
-              <Link className="a" to="/auth/register">
-                Registrarme
-              </Link>
-              <br></br>
-              <Link className="a" to="/" style={{ color: "#fff" }}>
-                Ir al inicio
-              </Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+                    <div className="register-link">
+                        <p>
+                            ¿No tienes una cuenta?{" "}
+                            <Link className="a" to="/auth/register">
+                                Registrarme
+                            </Link>
+                            <br></br>
+                            <Link className="a" to="/" style={{ color: "#fff" }}>
+                                Ir al inicio
+                            </Link>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 };
-
-/*<Link to="/auth/register"> ir a registro</Link>
-
-    <Link to="/">
-      <strong>Ir al inicio</strong>
-    </Link> */
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,57 +1,53 @@
 import { Link } from "react-router-dom";
+
 import { BiSolidLockAlt, BiSolidUser } from "react-icons/bi";
 import { IoMail } from "react-icons/io5";
+
 import "../styles/AuthStyles.css";
 
 export const RegisterPage = () => {
-  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Login Enviado");
-  };
+    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Login Enviado");
+    };
 
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <form action="submit" onSubmit={handleOnSubmit}>
-          <h1>GreenAlert</h1>
+    return (
+        <main className="container">
+            <div className="wrapper">
+                <form action="submit" onSubmit={handleOnSubmit}>
+                    <h1>GreenAlert</h1>
 
-          <div className="input-box">
-            <input type="text" placeholder="Nombre de Usuario" required></input>
-            <BiSolidUser className="icon" />
-          </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Nombre de Usuario" required></input>
+                        <BiSolidUser className="icon" />
+                    </div>
 
-          <div className="input-box">
-            <input
-              type="text"
-              placeholder="Correo electronico"
-              required
-            ></input>
-            <IoMail className="icon" />
-          </div>
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            placeholder="Correo electronico"
+                            required
+                        ></input>
+                        <IoMail className="icon" />
+                    </div>
 
-          <div className="input-box">
-            <input type="password" placeholder="Contraseña" required></input>
-            <BiSolidLockAlt className="icon" />
-          </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Contraseña" required></input>
+                        <BiSolidLockAlt className="icon" />
+                    </div>
 
-          <button type="submit">Registrarme</button>
+                    <button type="submit">Registrarme</button>
 
-          <div className="register-link">
-            <p>
-              ¿Ya tienes una cuenta?{" "}
-              <Link className="a" to="/auth/login">
-                Iniciar ahora
-              </Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+                    <div className="register-link">
+                        <p>
+                            ¿Ya tienes una cuenta?{" "}
+                            <Link className="a" to="/auth/login">
+                                Iniciar ahora
+                            </Link>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 };
-
-/* <div>
-            <p>Aca me voy a registrar</p>
-
-            <Link to='/auth/login' >Ir al login</Link>
-        </div> */
