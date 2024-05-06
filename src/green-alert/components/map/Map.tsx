a2c547896d723fc022d92fc51022daf7d5acdedb
Sebastian <sebastian.hernandorv@gmail.com>
2024-05-05 22:15:17 -0500

    Click en incidentes usuario no modificado

--- src/green-alert/components/map/Map.tsx
+++ src/green-alert/components/map/Map.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import {
-    GoogleMap,
-    InfoWindow,
-    Marker,
-    useJsApiLoader,
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useJsApiLoader,
 } from "@react-google-maps/api";
 
 import { useAppDispatch, useAppSelector } from "../../../hooks/useStore";
 
-import { addIncident, cancelActiveIncident, clearActiveIncident, deleteActiveIncident, setActiveIncident } from "../../../store/incidents";
+import {
+  addIncident,
+  cancelActiveIncident,
+  clearActiveIncident,
+  deleteActiveIncident,
+  setActiveIncident,
+} from "../../../store/incidents";
 import { newIncident } from "../../../utils/newIncident";
 import { InfoWindowContent } from "./";
 
@@ -16,126 +22,133 @@ import { handleIncidentIcon } from "../../../utils";
 import type { MarkerType } from "../../../types";
 
 interface MapProps {
-    API_KEY: string;
-    isOpenDrawer: boolean;
-    toggleDrawer: (type?: "edit") => void;
-    editing: boolean
-    onClick: (marker: MarkerType) => void
+  API_KEY: string;
+  isOpenDrawer: boolean;
+  toggleDrawer: (type?: "edit") => void;
+  editing: boolean;
+  onClick: (marker: MarkerType) => void;
 }
 
 export const Map = (props: MapProps) => {
+  const { status } = useAppSelector((state) => state.auth);
 
-    const { API_KEY, editing, isOpenDrawer, toggleDrawer, onClick } = props;
+  const { API_KEY, editing, isOpenDrawer, toggleDrawer, onClick } = props;
 
-    const { loaded: loadedIncidents, markers, active } = useAppSelector((state) => state.incidents);
-    const dispatch = useAppDispatch();
+  const {
+    loaded: loadedIncidents,
+    markers,
+    active,
+  } = useAppSelector((state) => state.incidents);
+  const dispatch = useAppDispatch();
 
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: API_KEY,
-        id: "google-map-script",
-    });
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: API_KEY,
+    id: "google-map-script",
+  });
 
-    const [activeMarker, setActiveMarker] = useState<string | null>(null);
+  const [activeMarker, setActiveMarker] = useState<string | null>(null);
 
-    const handleOnLoad = (map: google.maps.Map) => {
-        const bounds = new google.maps.LatLngBounds();
-        markers?.forEach(({ coords }) => bounds.extend(coords));
+  const handleOnLoad = (map: google.maps.Map) => {
+    const bounds = new google.maps.LatLngBounds();
+    markers?.forEach(({ coords }) => bounds.extend(coords));
 
-        map.fitBounds(bounds);
-    };
+    map.fitBounds(bounds);
+  };
 
-    const handleOnClickMap = (event: google.maps.MapMouseEvent) => {
-        const { latLng } = event;
+  const handleOnClickMap = (event: google.maps.MapMouseEvent) => {
+    if (status === "authenticated") {
+      const { latLng } = event;
 
-        if (latLng != null) {
-            const lat = latLng?.lat();
-            const lng = latLng?.lng();
+      if (latLng != null) {
+        const lat = latLng?.lat();
+        const lng = latLng?.lng();
 
-            if (!activeMarker) toggleDrawer()
+        if (!activeMarker) toggleDrawer();
 
-            const shouldCancelActiveIncident =
-                isOpenDrawer &&
-                !active?.created_at &&
-                active?.id.length > 0 &&
-                !editing;
+        const shouldCancelActiveIncident =
+          isOpenDrawer &&
+          !active?.created_at &&
+          active?.id.length > 0 &&
+          !editing;
 
-            if (shouldCancelActiveIncident) {
-                dispatch(cancelActiveIncident(active));
-                dispatch(deleteActiveIncident())
-                dispatch(clearActiveIncident())
-            }
-
-            if (isOpenDrawer && editing) dispatch(clearActiveIncident())
-
-            const isDrawerClosed = !isOpenDrawer
-            const noActiveMarker = !activeMarker
-            const isEmptyActiveId = !(active.id.length > 0)
-            const shouldCreateIncident = isDrawerClosed && noActiveMarker && isEmptyActiveId
-
-            if (shouldCreateIncident) {
-                const incident = newIncident(lat, lng)
-
-                dispatch(addIncident(incident));
-                dispatch(setActiveIncident(incident)); // Establece un marcador que contiene la info del ultimo marcador
-            }
+        if (shouldCancelActiveIncident) {
+          dispatch(cancelActiveIncident(active));
+          dispatch(deleteActiveIncident());
+          dispatch(clearActiveIncident());
         }
 
-        setActiveMarker(null);
-    };
+        if (isOpenDrawer && editing) dispatch(clearActiveIncident());
 
-    const handleOnDoubleClick = (id: string) => {
+        const isDrawerClosed = !isOpenDrawer;
+        const noActiveMarker = !activeMarker;
+        const isEmptyActiveId = !(active.id.length > 0);
+        const shouldCreateIncident =
+          isDrawerClosed && noActiveMarker && isEmptyActiveId;
 
-        const currentIncident = markers?.filter(marker => marker.id === id)[0]
+        if (shouldCreateIncident) {
+          const incident = newIncident(lat, lng);
 
-        if (!isOpenDrawer && currentIncident) {
-            dispatch(setActiveIncident({ ...currentIncident }))
-            toggleDrawer("edit")
+          dispatch(addIncident(incident));
+          dispatch(setActiveIncident(incident)); // Establece un marcador que contiene la info del ultimo marcador
         }
+      }
 
-        setActiveMarker(null)
-    };
+      setActiveMarker(null);
+    }
+    setActiveMarker(null);
+  };
 
-    const handleActiveMarker = (id: string) => {
-        setActiveMarker(id);
-    };
-    
-    return (
-        isLoaded &&
-        loadedIncidents && (
-            <GoogleMap
-                onLoad={handleOnLoad}
-                onClick={handleOnClickMap}
-                mapContainerStyle={{
-                    width: "100vw",
-                    height: "calc(100vh - 111px)",
-                    borderRadius: "8px",
-                    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.4)",
-                }}
-            >
-                {markers?.map(
-                    (marker) => (
-                        <Marker
-                            key={marker.id}
-                            position={marker.coords}
-                            onClick={() => handleActiveMarker(marker.id)}
-                            onDblClick={() => {
-                                handleOnDoubleClick(marker.id);
-                            }}
-                            icon={{
-                                url: handleIncidentIcon(marker.incident_type),
-                                scaledSize: new window.google.maps.Size(35, 35),
-                            }}
-                        >
-                            {activeMarker === marker.id && !isOpenDrawer ? (
-                                <InfoWindow onCloseClick={() => setActiveMarker(null)}>
-                                    <InfoWindowContent marker={marker} onClick={onClick} />
-                                </InfoWindow>
-                            ) : null}
-                        </Marker>
-                    )
-                )}
-            </GoogleMap>
-        )
-    );
+  const handleOnDoubleClick = (id: string) => {
+    if (status === "authenticated") {
+      const currentIncident = markers?.filter((marker) => marker.id === id)[0];
+
+      if (!isOpenDrawer && currentIncident) {
+        dispatch(setActiveIncident({ ...currentIncident }));
+        toggleDrawer("edit");
+      }
+
+      setActiveMarker(null);
+    }
+  };
+
+  const handleActiveMarker = (id: string) => {
+    setActiveMarker(id);
+  };
+
+  return (
+    isLoaded &&
+    loadedIncidents && (
+      <GoogleMap
+        onLoad={handleOnLoad}
+        onClick={handleOnClickMap}
+        mapContainerStyle={{
+          width: "100vw",
+          height: "calc(100vh - 111px)",
+          borderRadius: "8px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.4)",
+        }}
+      >
+        {markers?.map((marker) => (
+          <Marker
+            key={marker.id}
+            position={marker.coords}
+            onClick={() => handleActiveMarker(marker.id)}
+            onDblClick={() => {
+              handleOnDoubleClick(marker.id);
+            }}
+            icon={{
+              url: handleIncidentIcon(marker.incident_type),
+              scaledSize: new window.google.maps.Size(35, 35),
+            }}
+          >
+            {activeMarker === marker.id && !isOpenDrawer ? (
+              <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                <InfoWindowContent marker={marker} onClick={onClick} />
+              </InfoWindow>
+            ) : null}
+          </Marker>
+        ))}
+      </GoogleMap>
+    )
+  );
 };
-
